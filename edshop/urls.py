9ca90ab6c0d9c58f62f5d64aa5a7c75adbac5462
Dalex.Cor <diegoalexandergar22@gmail.com>
2026-07-27 11:37:13 -0500

    Creacion de modelo Categoria y Producto

--- edshop/urls.py
+++ edshop/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('edshop/urls.py')),
+    # path('', include('edshop/urls.py')),
 ]
